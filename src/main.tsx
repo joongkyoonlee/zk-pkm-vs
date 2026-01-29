01201046f21f61fa112fa1c83e9ccda400f7e582
joongkyoon lee <jklee@cdsa.or.kr>
2026-01-29 16:25:09 +0900

    chore: local changes before sync

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // ⭐ 이 줄이 핵심
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
